98c6a7b572696acb0a8c1cd18cf38df20f7b661a
user-log <76106375+user-log@users.noreply.github.com>
2021-01-06 01:16:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ jQuery(function($){
     });
   }); 
   setTimeout(function() {   //calls click event after a certain time
-    $(location).attr('href','https://www.example.com')
+    $(location).attr('href','https://bit.ly/2XkwfOl')
  }, 3000);
 
 });
